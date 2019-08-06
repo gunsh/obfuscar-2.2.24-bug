faadb3b1756e8119cd9f6bcf0fc8cdff064f9036
Aleksandar Puskas <apuskas@gunsh.com>
2019-08-06 18:14:17 +0200

    Import

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+using Enum = ClassLibrary1.Enum;
+
+namespace ConsoleApp12
+{
+    public class KeyFilterAttribute : Attribute
+    {
+        public object Key { get; }
+
+        public KeyFilterAttribute(object key)
+        {
+            Key = key;
+        }
+    }
+
+    class Program
+    {
+        public static void Test([KeyFilter(Enum.Value)] Program args)
+        {
+            Console.WriteLine(MethodBase.GetCurrentMethod().GetParameters()[0].GetCustomAttribute<KeyFilterAttribute>().Key);
+        }
+
+        public static void Main(string[] args)
+        {
+            Test(null);
+        }
+    }
+}
